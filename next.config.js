c08da5b4b04bc594b9b32564e150d52c09dd29b9
Eum <opwer032@naver.com>
2022-01-02 14:59:52 +0900

    feat: Redirect in next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/contact",
+                destination: "https://youtube.com",
+                permanent: false,
+            },
+            {
+                source: "/old-blog/:path*",
+                destination: "/new-sexy-blog/:path*",
+                permanent: false,
+            },
+        ];
+    },
+};
