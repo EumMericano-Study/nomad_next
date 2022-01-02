cda712e7b8156cf8fb8a0c722de4651ba301fc70
Eum <opwer032@naver.com>
2022-01-02 15:06:38 +0900

    feat: Add rewrite for hide api information on next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
     reactStrictMode: true,
     async redirects() {
@@ -12,6 +15,14 @@ module.exports = {
                 source: "/old-blog/:path*",
                 destination: "/new-sexy-blog/:path*",
                 permanent: false,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/movies",
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
             },
         ];
     },
