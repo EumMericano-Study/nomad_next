9023d73a8e936b831365d913ba6c3d0123e8e533
Eum <opwer032@naver.com>
2022-01-02 13:49:12 +0900

    feat: Fetch popular movies data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home() {
+    const [movies, setMovies] = useState<MovieIState[]>();
+    useEffect(() => {
+        (async () => {
+            const { results }: { results: MovieIState[] } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <div>
             <Seo title="Home" />
-            <h2>home</h2>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie: MovieIState) => {
+                return <div key={movie.id}>{movie.original_title}</div>;
+            })}
         </div>
     );
 }
