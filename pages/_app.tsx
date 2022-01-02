244bef3c738f51912a546bf50f58229e6db08738
Eum <opwer032@naver.com>
2022-01-02 10:45:42 +0900

    style: Set global css in _app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar";
 
+import "../styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
